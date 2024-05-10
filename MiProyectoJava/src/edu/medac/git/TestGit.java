cb8b7e2b56289b44f8754f12984f04e1ced6b609
Guillermo Cantero <Guillermo Cantero@DESKTOP-P57PRA3>
2024-05-10 08:43:51 +0200

    Comit inicial del proyecto

---
+++ MiProyectoJava/src/edu/medac/git/TestGit.java
@@ -0,0 +1,10 @@
+package edu.medac.git;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.print("*** Imprimiendo desde TestGit");
+
+	}
+
+}
---
+++ MiProyectoJava/src/edu/medac/git/TestGitDos.java
@@ -0,0 +1,10 @@
+package edu.medac.git;
+
+public class TestGitDos {
+
+	public static void main(String[] args) {
+		System.out.print("*** Imprimiendo desde TestGitDos");
+
+	}
+
+}
